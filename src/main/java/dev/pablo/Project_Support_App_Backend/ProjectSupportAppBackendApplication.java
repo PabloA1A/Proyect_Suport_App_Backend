a4e714f57b88deb4b56fccf6328cac8ed0fe35f7
PabloA1A <pablo_abadalvarez@hotmail.es>
2024-07-15 21:37:32 +0200

    init project spring boot

---
+++ src/main/java/dev/pablo/Project_Support_App_Backend/ProjectSupportAppBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.pablo.Project_Support_App_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSupportAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSupportAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/pablo/Project_Support_App_Backend/ProjectSupportAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pablo.Project_Support_App_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSupportAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
